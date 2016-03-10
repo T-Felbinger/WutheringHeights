9d778239f6eedf1937faa82977882d169c429389
David Thames <davidc.thames@gmail.com>
2016-03-09 19:53:36 -0500

    Added range input at top and multiple pages of timeline

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 $(document).ready(function() {
-function getRandomSize(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
+    changeTime(0);
+    function getRandomSize(min, max) {
+      return Math.round(Math.random() * (max - min) + min);
+    }
   for (var i = 0; i < 25; i++) {
     var width = getRandomSize(200, 400);
     var height =  getRandomSize(200, 400);
-    $('#photos').append('<img src="http://www.lorempixel.com/'+width+'/'+height+'/cats" alt="pretty kitty">');
+    $('.photos').append('<img src="http://www.lorempixel.com/'+width+'/'+height+'/cats" alt="pretty kitty">');
   }
 });
+
+function changeTime(i) {
+    $(".timeline").css("opacity", "0").css("z-index", "-1");
+    $(".timeline:eq("+i+")").css("opacity", "1").css("z-index", "1");
+    
+    $(".photos").css("opacity", "0").css("z-index", "-10000");
+    $(".photos:eq("+i+")").css("opacity", "1").css("z-index", "-1000");
+    
+}
+
+
+
