1ea6e9e95c38e2a88500130ae437276895267a27
David Thames <davidcthames@gmail.com>
2016-03-10 10:45:50 -0500

    Fixed image top position and added more tabs of timemap

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-$(document).ready(function() {
-    changeTime(0);
-    setImages();
+$(document).ready(function () {
+	changeTime(0);
+	setImages();
 });
 
 function setImages() {
-    for (var time = 0; time < $(".timeline").length; time++) {
-      for (var i = 0; i < 8; i++) {
+	for (var time = 0; time < $(".timeline").length; time++) {
+		for (var a = 0; a < 3; a++) {
+			for (var i = 1; i <= 8; i++) {
 
-        $('.photos:eq(' + time + ')').append('<img src="' + pickImage(time, i) + '">');
-      }
-    }
+				$('.photos:eq(' + time + ')').append('<img src="' + pickImage(time, i) + '">');
+			}
+		}
+	}
 }
 
 function changeTime(i) {
-    $(".timeline").css("opacity", "0").css("z-index", "-1");
-    $(".timeline:eq("+i+")").css("opacity", "1").css("z-index", "1");
-    
-    $(".photos").css("opacity", "0").css("z-index", "-10000");
-    $(".photos:eq("+i+")").css("opacity", "1").css("z-index", "-1000");
-    
-    
+	$(".timeline").css("opacity", "0").css("z-index", "-1");
+	$(".timeline:eq(" + i + ")").css("opacity", "1").css("z-index", "1");
+
+	$(".photos").css("opacity", "0").css("z-index", "-10000");
+	$(".photos:eq(" + i + ")").css("opacity", "1").css("z-index", "-1000");
+
+
 }
 
 var dates = ["1757", "1760", "1770", "1780"];
 
 function pickImage(time, i) {
-    return "Images/" + dates[time] + "s/img " + i + ".jpg";
+	return "Images/" + dates[time] + "s/img " + i + ".jpg";
 }
-
